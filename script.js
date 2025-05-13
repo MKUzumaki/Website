dc7ccf0902a6da48175068e7e4bc8874aab9eb75
Makara Chan <77384617+MKUzumaki@users.noreply.github.com>
2025-05-13 15:45:25 +0700

    java and htlm hiring fix

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
 }
+document.getElementById('hireBtn').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // Let the user choose:
+  const choice = prompt(
+    "How would you like to send your message?\n\n" +
+    "1: Use my mail client (default)\n" +
+    "2: Use Gmail in browser\n\n" +
+    "Type 1 or 2, then OK."
+  );
+
+  if (choice === '2') {
+    // Web-mail: Gmail compose
+    window.open(
+      'https://mail.google.com/mail/?view=cm&fs=1' +
+      '&to=makara.chan@mail.fr' +
+      '&su=' + encodeURIComponent('Hiring Inquiry'),
+      '_blank', 'noopener'
+    );
+  } else {
+    // Default: launch local mail client
+    window.location.href =
+      'mailto:makara.chan@mail.fr' +
+      '?subject=' + encodeURIComponent('Hiring Inquiry');
+  }
+});
